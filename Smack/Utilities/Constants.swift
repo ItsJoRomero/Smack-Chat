5f0a233600525899e115dd28959758589b19f64c
Johanna Romero <johanna.romero@icloud.com>
2017-08-12 21:49:44 -0400

    Lesson 4

---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Johanna Romero on 8/12/17.
+//  Copyright © 2017 Johanna Romero. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
