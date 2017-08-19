c8d558bbb93041a2bec347c01d7208f10c7e0f2b
Johanna Romero <johanna.romero@icloud.com>
2017-08-19 18:28:02 -0400

    Lesson 13

---
+++ Smack/View/AvatarCell.swift
@@ -0,0 +1,26 @@
+//
+//  AvatarCell.swift
+//  Smack
+//
+//  Created by Johanna Romero on 8/19/17.
+//  Copyright © 2017 Johanna Romero. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpView()
+    }
+    
+    func setUpView() {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+    
+}
